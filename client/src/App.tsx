9b75711931078022e2daf764a8033411d18ef406
icallitnewart <icallitnewart@gmail.com>
2024-06-10 03:58:22 +0900

    [FE] Chore: 라우팅 초기 설정 및 회원가입 페이지 연결
    설명
    - 라우팅 설정
    - 회원가입 페이지 라우터 연결
    
    Related to: #4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import GlobalStyles from './GlobalStyles';
+import RegisterPage from './pages/RegisterPage';
 
-function App() {
+function App(): JSX.Element {
 	return (
 		<div className="App">
-			<GlobalStyles />
+			<BrowserRouter>
+				<GlobalStyles />
+				<Routes>
+					<Route path="/" element={<div>Home</div>} />
+					<Route path="/user/register" element={<RegisterPage />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
