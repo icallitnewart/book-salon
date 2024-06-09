52e71c6054df4ffcffc571d6ca903bf9f51de650
icallitnewart <icallitnewart@gmail.com>
2024-06-10 05:21:10 +0900

    [FE] Chore: 회원정보조회 페이지 라우터 연결
    설명
    - 회원정보조회 페이지 라우터 연결
    
    Related to: #4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import GlobalStyles from './GlobalStyles';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import UserEditPage from './pages/UserEditPage';
+import UserInfoPage from './pages/UserInfoPage';
 
 function App(): JSX.Element {
 	return (
@@ -15,6 +16,7 @@ function App(): JSX.Element {
 					<Route path="/" element={<div>Home</div>} />
 					<Route path="/user/register" element={<RegisterPage />} />
 					<Route path="/user/login" element={<LoginPage />} />
+					<Route path="/user/info" element={<UserInfoPage />} />
 					<Route path="/user/edit" element={<UserEditPage />} />
 				</Routes>
 			</BrowserRouter>
