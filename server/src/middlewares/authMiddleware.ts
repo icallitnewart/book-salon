ea29cd8594cb8f261ebd9bc48ed179400e477ad3
icallitnewart <icallitnewart@gmail.com>
2024-07-01 14:44:47 +0900

    [BE] Refactor: 인증 미들웨어에 isRequired 조건 추가 및 토큰 만료 에러 처리
    설명
    - 토큰이 필수가 아닌 경우, 에러를 발생시키지 않도록 isRequired 조건 추가
    - 만료된 토큰인 경우 에러 처리 (토큰이 필수가 아니면 에러 발생하지 않음)
    
    부가설명
    - 이는 비로그인 상태가 비정상적인 상황이 아닌 경우에 사용하기 위함 (예: 인증 정보 조회)
    
    Related to: #5

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -1,34 +1,53 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
+import jwt, { JwtPayload, TokenExpiredError } from 'jsonwebtoken';
 
 import { HttpError } from '../utils/HttpError';
 
-const authMiddleware = async (
-	req: Request,
-	res: Response,
-	next: NextFunction,
-) => {
-	const request = req;
-	const token = request.cookies?.token;
+const authMiddleware =
+	(isRequired: boolean = true): RequestHandler =>
+	async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+		const request = req;
+		const token = request.cookies?.token;
 
-	if (!token) {
-		next(new HttpError('로그인이 필요한 서비스입니다.', 401, 'auth'));
-		return;
-	}
+		if (!token) {
+			if (isRequired) {
+				return next(
+					new HttpError('로그인이 필요한 서비스입니다.', 401, 'auth'),
+				);
+			}
 
-	try {
-		const { userId } = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
-
-		if (!userId) {
-			next(new HttpError('인증에 실패하였습니다.', 401, 'auth'));
-			return;
+			return next();
 		}
 
-		request.userId = userId;
-		next();
-	} catch (error) {
-		next(new HttpError('인증에 실패하였습니다.', 401, 'auth'));
-	}
-};
+		try {
+			const { userId } = jwt.verify(
+				token,
+				process.env.JWT_SECRET!,
+			) as JwtPayload;
+
+			if (!userId) {
+				return next(new HttpError('유효하지 않은 토큰입니다.', 401, 'auth'));
+			}
+
+			request.userId = userId;
+			return next();
+		} catch (error) {
+			if (error instanceof TokenExpiredError) {
+				if (isRequired) {
+					return next(
+						new HttpError(
+							'인증이 만료되었습니다. 다시 로그인해 주세요.',
+							401,
+							'auth',
+						),
+					);
+				}
+
+				return next();
+			}
+
+			return next(new HttpError('인증에 실패하였습니다.', 401, 'auth'));
+		}
+	};
 
 export default authMiddleware;
