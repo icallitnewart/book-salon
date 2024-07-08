01bfac726d2343e60fab638012c0f698fc5090e5
icallitnewart <icallitnewart@gmail.com>
2024-07-08 16:42:31 +0900

    [BE] Feat: 테스트용 도서 상세 정보 조회 API 개발
    설명
    - ISBN을 이용한 도서 상세 정보 조회 API 테스트
    
    참고사항
    - ISBN 고정
    - 제거 예정
    
    Related to: #9

--- server/src/domains/book/bookRouter.ts
+++ server/src/domains/book/bookRouter.ts
@@ -12,4 +12,13 @@ router.get('/test/bestseller', async (req, res) => {
 	res.json({ data: response.data });
 });
 
+// TODO: 제거 예정 (테스트용)
+router.get('/test/detail', async (req, res) => {
+	const TTBKey = process.env.TTB_KEY;
+	const response = await axios.get(
+		`http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?ttbkey=${TTBKey}&itemIdType=ISBN&ItemId=9791189327156&output=js&Version=20131101&OptResult=ebookList,usedList,reviewList&Cover=Big`,
+	);
+	res.json({ data: response.data });
+});
+
 export default router;
