d6458ffc484ae5d455a2d797e1df8c2e95645dde
icallitnewart <icallitnewart@gmail.com>
2024-07-22 17:03:18 +0900

    [FE] Fix: 로그인시 의도치 않게 페이지 리다이렉트되는 문제 해결
    문제
    - 로그인 페이지에서 로그인 성공시 이전 페이지로 리다이렉트되어야 하는데, 메인 페이지로 리다이렉트됨
    
    원인
    - 로그인 성공시 redux 상태로 인증 정보가 업데이트 완료되기 전에 이전 페이지로 리다이렉트가 먼저 실행됨
    - 리다이렉트된 페이지가 Private 페이지인 경우, 아직 로그인되지 않은 상태로 인식하여 메인 페이지로 다시 리다이렉트됨
    
    해결방법
    - redux 상태로 업데이트하지 않고, 로그인시 캐시에 저장한 인증 정보를 가져와서 사용함
    
    Related to: #19, c9ca33ba44d482519aa3d520fd39393a713de90f

--- client/src/shared/components/organisms/AuthChecker.tsx
+++ client/src/shared/components/organisms/AuthChecker.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { useAppSelector } from '@redux/store';
 
 import { ROUTES } from '@constants/routes';
 import { AUTH_TYPES } from '@constants/auth';
+import { queryClient } from '@config/query/queryClient';
+import { userKeys } from '@config/query/queryKeys';
+import { IAuthQueryData } from '@features/user/types/userQueryData';
 
-interface IAuthProps {
+interface IAuthCheckerProps {
 	children: JSX.Element;
 	type: keyof typeof AUTH_TYPES;
 }
 
-function AuthChecker({ children, type }: IAuthProps): JSX.Element {
+function AuthChecker({ children, type }: IAuthCheckerProps): JSX.Element {
 	const location = useLocation();
-	const isAuth = useAppSelector(state => state.user.isAuth);
+	const authData = queryClient.getQueryData<IAuthQueryData>(userKeys.auth);
+	const isAuth = authData?.isAuth;
 
 	if (type === AUTH_TYPES.PRIVATE && !isAuth) {
 		return (
 			<Navigate to={ROUTES.USER.LOGIN} state={{ from: location }} replace />
 		);
 	}
+
 	if (type === AUTH_TYPES.GUEST && isAuth) {
 		return <Navigate to={ROUTES.MAIN} replace />;
 	}
