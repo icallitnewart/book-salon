2b80191290e37e6586be1c559e6c11ab9619a21b
icallitnewart <icallitnewart@gmail.com>
2024-07-23 12:42:45 +0900

    [FE] Refactor: 로그아웃 기능 성공 및 에러 처리 컴포넌트 내에서 실행하도록 변경
    설명
    - UI 로직과 데이터 페칭 로직 분리를 위해 useLogoutUser 훅에서 진행하던 성공 및 에러 처리를 컴포넌트에서 하도록 변경
    - 훅에서 쿼리 데이터 초기화 함수 반환하여 컴포넌트 내에서 로그아웃 성공시 사용
    
    Related to: #19

--- client/src/features/user/components/atoms/UserLogoutButton.tsx
+++ client/src/features/user/components/atoms/UserLogoutButton.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { ROUTES } from '@constants/routes';
+import useLogoutUser from '@features/user/hooks/useLogoutUser';
+import { handleApiError } from '@utils/errorHandler';
 
 import { SubtleButton } from '@buttons';
-import useLogoutUser from '@features/user/hooks/useLogoutUser';
 
 function UserLogoutButton() {
-	const { mutate: logoutUser } = useLogoutUser();
+	const navigate = useNavigate();
+	const { logoutUser, initialiseQueriesAfterMutation } = useLogoutUser();
+
+	const handleLogout = () => {
+		logoutUser(undefined, {
+			onSuccess: () => {
+				initialiseQueriesAfterMutation();
+				alert('로그아웃 되었습니다.');
+				navigate(ROUTES.MAIN);
+			},
+			onError: err => {
+				const error = handleApiError(err);
+
+				if (error) {
+					alert('로그아웃에 실패하였습니다. 다시 시도해주세요.');
+				}
+			},
+		});
+	};
 
 	return (
-		<SubtleButton type="button" onClick={logoutUser}>
+		<SubtleButton type="button" onClick={handleLogout}>
 			로그아웃
 		</SubtleButton>
 	);
--- client/src/features/user/hooks/useLogoutUser.ts
+++ client/src/features/user/hooks/useLogoutUser.ts
@@ -1,31 +1,37 @@
-import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 
 import { userKeys } from '@config/query/queryKeys';
 import { queryClient } from '@config/query/queryClient';
-import { ROUTES } from '@constants/routes';
-import { handleApiError } from '@utils/errorHandler';
 import userApis from '../apis/userApis';
 
 function useLogoutUser() {
-	const navigate = useNavigate();
 	const mutation = useMutation({
 		mutationKey: userKeys.logout,
 		mutationFn: userApis.logout,
-		onSuccess: () => {
-			alert('로그아웃 되었습니다.');
-			navigate(ROUTES.MAIN);
-			queryClient.removeQueries({ queryKey: userKeys.all });
-		},
-		onError: err => {
-			const error = handleApiError(err);
-			if (error) {
-				alert('로그아웃에 실패하였습니다. 다시 시도해주세요.');
-			}
-		},
 	});
 
-	return mutation;
+	const removeUserQueries = () => {
+		queryClient.removeQueries({
+			queryKey: userKeys.all,
+		});
+	};
+
+	const initialiseAuthQueryData = () => {
+		queryClient.setQueryData(userKeys.auth, {
+			isAuth: false,
+		});
+	};
+
+	const initialiseQueriesAfterMutation = () => {
+		removeUserQueries();
+		initialiseAuthQueryData();
+	};
+
+	return {
+		...mutation,
+		logoutUser: mutation.mutate,
+		initialiseQueriesAfterMutation,
+	};
 }
 
 export default useLogoutUser;
