54e4e1e18cd82e0f5f318c3336204db59feecc04
icallitnewart <icallitnewart@gmail.com>
2024-07-26 10:14:43 +0900

    [FE] Feat: 도서 프로필 패널에 도서 상세 정보 조회 API 연동
    설명
    - DB(props) 혹은 알라딘 API에서 도서 정보를 가져와 도서 프로필 패널에 데이터 바인딩
    - props로 받은 DB 데이터가 없다면 ISBN으로 알라딘 API 호출 (혹은 이전에 캐싱된 데이터 사용)
    - 도서 정보 데이터 출처 우선 순위 1) DB 2) 알라딘 API
    - 로딩 및 에러 처리는 적용해놓았으나 추후 수정 필요
    
    추가 설명
    - 도서 프로필 패널은 도서 리뷰 상세 페이지와 작성 페이지에 사용되므로 도서 정보 데이터 출처가 다름 (상세: DB / 작성: 알라딘 API)
    
    Related to: #20

--- client/src/features/book/components/organisms/BookProfilePanel.tsx
+++ client/src/features/book/components/organisms/BookProfilePanel.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { styled } from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
-import { IBookInfo } from '@features/book/types/bookDetail';
+import { IBookProfile } from '@features/book/types/bookProfile';
+import { ROUTES } from '@constants/routes';
 
+import useBookDetail from '@features/book/hooks/useBookDetail';
+
+import withAsyncBoundary from '@components/organisms/withAsyncBoundary';
 import { Heading2 as BookTitle } from '@typographies';
 import BookCoverWithBackground from '@features/book/components/molecules/BookCoverWithBackground';
 import BookInfoTextWithLabel from '@features/book/components/molecules/BookInfoTextWithLabel';
@@ -28,45 +32,51 @@ const BookInfoDescription = styled(BookInfoTextWithLabel).attrs({
 	margin: '4px 0px',
 })``;
 
-const sample: IBookInfo = {
-	title:
-		'물고기는 존재하지 않는다 - 상실, 사랑 그리고 숨어 있는 삶의 질서에 관한 이야기',
-	author: '룰루 밀러 (지은이), 정지인 (옮긴이)',
-	cover:
-		'https://image.aladin.co.kr/product/28465/73/cover200/k092835920_2.jpg',
-	category: '국내도서 > 과학 > 기초과학/교양과학',
-	publisher: '곰출판',
-	pubDate: '2021-12-17',
-};
+interface IBookProfilePanelProps {
+	dbBook?: IBookProfile;
+}
 
-function BookProfilePanel(): JSX.Element {
+function BookProfilePanel({ dbBook }: IBookProfilePanelProps): JSX.Element {
+	const { isbn } = useParams();
+	const { data: fetchedBook, isPending } = useBookDetail(
+		dbBook?.isbn ? undefined : isbn,
+	);
+	const book = useMemo(() => dbBook ?? fetchedBook, [dbBook, fetchedBook]);
+
+	if (!dbBook && isPending) {
+		// TODO: Skeleton UI로 대체
+		return <div>Loading...</div>;
+	}
+
 	return (
 		<Container>
-			<Link to="/">
+			<Link to={ROUTES.BOOK.DETAIL(book?.isbn)}>
 				<BookCoverWithBackground
-					src={sample.cover}
-					alt={sample.title}
+					src={book?.cover}
+					alt={book?.title}
 					$width="100%"
 					$imgWidth="45%"
 				/>
 			</Link>
 			<BookInfoTextBox>
-				<Link to="/">
+				<Link to={ROUTES.BOOK.DETAIL(book?.isbn)}>
 					<BookTitle
 						variant="card-title-lg"
 						$margin="15px 0 7px"
 						$lineHeight={1.8}
 					>
-						{sample.title}
+						{book?.title}
 					</BookTitle>
 				</Link>
-				<BookInfoDescription label="저자" text={sample.author} />
-				<BookInfoDescription label="카테고리" text={sample.category} />
-				<BookInfoDescription label="출판사" text={sample.publisher} />
-				<BookInfoDescription label="출판일" text={sample.pubDate} />
+				<BookInfoDescription label="저자" text={book?.author} />
+				<BookInfoDescription label="카테고리" text={book?.category} />
+				<BookInfoDescription label="출판사" text={book?.publisher} />
+				<BookInfoDescription label="출판일" text={book?.pubDate} />
 			</BookInfoTextBox>
 		</Container>
 	);
 }
 
-export default BookProfilePanel;
+export default withAsyncBoundary(BookProfilePanel, {
+	SuspenseFallback: null,
+});
--- client/src/features/book/types/bookProfile.ts
+++ client/src/features/book/types/bookProfile.ts
@@ -4,3 +4,13 @@ export interface IBookProfilePreview {
 	cover: string;
 	link?: string;
 }
+
+export interface IBookProfile {
+	title: string;
+	author: string;
+	cover: string;
+	category: string;
+	publisher: string;
+	pubDate: string;
+	isbn: string;
+}
