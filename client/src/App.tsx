22c6fd5f77b7a9238686443cfd0b64686accb16d
icallitnewart <icallitnewart@gmail.com>
2024-08-01 09:48:09 +0900

    [FE] Feat: 페이지 이동시 스크롤 위치 초기화
    설명
    - 새로운 페이지로 이동시 스크롤 위치 초기화 커스텀훅 useScrollToTop 생성 (뒤로 가기는 제외)
    - 모든 Route에 useScrollToTop 훅 적용
    
    참고사항
    - 라우팅 로직 새로운 컴포넌트로 분리
    
    Related to: #22

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,101 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import { AUTH_TYPES } from '@constants/auth';
-import { ROUTES } from '@constants/routes';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from 'GlobalStyles';
-import AuthChecker from '@components/organisms/AuthChecker';
-
-import MainPage from '@pages/MainPage';
-import RegisterPage from '@pages/RegisterPage';
-import LoginPage from '@pages/LoginPage';
-import MyProfilePage from '@pages/MyProfilePage';
-import ProfileEditPage from '@pages/ProfileEditPage';
-import BookDetailPage from '@pages/BookDetailPage';
-import ReviewDetailPage from '@pages/ReviewDetailPage';
-import ReviewAddPage from '@pages/ReviewAddPage';
-import ReviewEditPage from '@pages/ReviewEditPage';
+import AppRoutes from 'AppRoutes';
 
 function App(): JSX.Element {
 	return (
 		<div className="App">
 			<BrowserRouter>
 				<GlobalStyles />
-				<Routes>
-					<Route
-						path={ROUTES.MAIN}
-						element={
-							<AuthChecker type={AUTH_TYPES.PUBLIC}>
-								<MainPage />
-							</AuthChecker>
-						}
-					/>
-					<Route
-						path={ROUTES.USER.REGISTER}
-						element={
-							<AuthChecker type={AUTH_TYPES.GUEST}>
-								<RegisterPage />
-							</AuthChecker>
-						}
-					/>
-					<Route
-						path={ROUTES.USER.LOGIN}
-						element={
-							<AuthChecker type={AUTH_TYPES.GUEST}>
-								<LoginPage />
-							</AuthChecker>
-						}
-					/>
-					<Route
-						path={ROUTES.USER.MY_PROFILE}
-						element={
-							<AuthChecker type={AUTH_TYPES.PRIVATE}>
-								<MyProfilePage />
-							</AuthChecker>
-						}
-					/>
-					<Route
-						path={ROUTES.USER.PROFILE_EDIT}
-						element={
-							<AuthChecker type={AUTH_TYPES.PRIVATE}>
-								<ProfileEditPage />
-							</AuthChecker>
-						}
-					/>
-					<Route
-						path={ROUTES.BOOK.DETAIL()}
-						element={
-							<AuthChecker type={AUTH_TYPES.PUBLIC}>
-								<BookDetailPage />
-							</AuthChecker>
-						}
-					/>
-					<Route
-						path={ROUTES.REVIEW.DETAIL()}
-						element={
-							<AuthChecker type={AUTH_TYPES.PUBLIC}>
-								<ReviewDetailPage />
-							</AuthChecker>
-						}
-					/>
-					<Route
-						path={ROUTES.REVIEW.ADD()}
-						element={
-							<AuthChecker type={AUTH_TYPES.PRIVATE}>
-								<ReviewAddPage />
-							</AuthChecker>
-						}
-					/>
-					<Route
-						path={ROUTES.REVIEW.EDIT()}
-						element={
-							<AuthChecker type={AUTH_TYPES.PRIVATE}>
-								<ReviewEditPage />
-							</AuthChecker>
-						}
-					/>
-				</Routes>
+				<AppRoutes />
 			</BrowserRouter>
 		</div>
 	);
---
+++ client/src/AppRoutes.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { ROUTES } from '@constants/routes';
+import { AUTH_TYPES } from '@constants/auth';
+
+import useScrollToTop from '@hooks/useScrollToTop';
+import AuthChecker from '@components/organisms/AuthChecker';
+
+import MainPage from '@pages/MainPage';
+import RegisterPage from '@pages/RegisterPage';
+import LoginPage from '@pages/LoginPage';
+import MyProfilePage from '@pages/MyProfilePage';
+import ProfileEditPage from '@pages/ProfileEditPage';
+import BookDetailPage from '@pages/BookDetailPage';
+import ReviewDetailPage from '@pages/ReviewDetailPage';
+import ReviewAddPage from '@pages/ReviewAddPage';
+import ReviewEditPage from '@pages/ReviewEditPage';
+
+const routes = [
+	{
+		name: '메인',
+		path: ROUTES.MAIN,
+		authType: AUTH_TYPES.PUBLIC,
+		element: <MainPage />,
+	},
+	{
+		name: '회원가입',
+		path: ROUTES.USER.REGISTER,
+		authType: AUTH_TYPES.GUEST,
+		element: <RegisterPage />,
+	},
+	{
+		name: '로그인',
+		path: ROUTES.USER.LOGIN,
+		authType: AUTH_TYPES.GUEST,
+		element: <LoginPage />,
+	},
+	{
+		name: '내 프로필 조회',
+		path: ROUTES.USER.MY_PROFILE,
+		authType: AUTH_TYPES.PRIVATE,
+		element: <MyProfilePage />,
+	},
+	{
+		name: '회원 정보 수정',
+		path: ROUTES.USER.PROFILE_EDIT,
+		authType: AUTH_TYPES.PRIVATE,
+		element: <ProfileEditPage />,
+	},
+	{
+		name: '도서 상세',
+		path: ROUTES.BOOK.DETAIL(),
+		authType: AUTH_TYPES.PUBLIC,
+		element: <BookDetailPage />,
+	},
+	{
+		name: '리뷰 상세',
+		path: ROUTES.REVIEW.DETAIL(),
+		authType: AUTH_TYPES.PUBLIC,
+		element: <ReviewDetailPage />,
+	},
+	{
+		name: '리뷰 작성',
+		path: ROUTES.REVIEW.ADD(),
+		authType: AUTH_TYPES.PRIVATE,
+		element: <ReviewAddPage />,
+	},
+	{
+		name: '리뷰 수정',
+		path: ROUTES.REVIEW.EDIT(),
+		authType: AUTH_TYPES.PRIVATE,
+		element: <ReviewEditPage />,
+	},
+];
+
+function AppRoutes() {
+	useScrollToTop();
+
+	return (
+		<Routes>
+			{routes.map(({ name, path, authType, element }) => (
+				<Route
+					key={name}
+					path={path}
+					element={<AuthChecker type={authType}>{element}</AuthChecker>}
+				/>
+			))}
+		</Routes>
+	);
+}
+
+export default AppRoutes;
