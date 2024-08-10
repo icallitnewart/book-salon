e756938d6d3eeea15746712d1f04c147dae2f950
icallitnewart <icallitnewart@gmail.com>
2024-08-10 18:55:04 +0900

    [FE] Design: 검색 모달창 단축키 안내 UI 개발
    설명
    - 키보드로 검색 결과를 이동 혹은 선택하거나 모달창을 닫는 방법을 알려주는 단축키 안내 레이아웃 구현 및 디자인 작업
    
    Related to: #25

--- client/src/shared/components/organisms/SearchModal.tsx
+++ client/src/shared/components/organisms/SearchModal.tsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import { styled } from 'styled-components';
 
+import { ParagraphWithStyles } from '@typographies';
 import ModalSearchBar from './ModalSearchBar';
 
 const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 	width: 600px;
 	height: 550px;
 
 	background-color: #fff;
 `;
 
+const ShortcutInfo = styled(ParagraphWithStyles)`
+	display: flex;
+	align-items: center;
+	width: 100%;
+	height: 30px;
+	padding-bottom: 1px;
+
+	border-top: 1px solid #ddd;
+	box-shadow: -2px 0px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const InfoText = styled.span`
+	padding: 0px 10px;
+
+	&:not(:last-child) {
+		border-right: 1px solid #ddd;
+	}
+`;
+
 function SearchModal(): JSX.Element {
 	return (
 		<Container>
 			<ModalSearchBar />
+			<ShortcutInfo variant="list-meta-sm" $color="#888">
+				<InfoText>[↑or↓] 이동</InfoText>
+				<InfoText>[Enter] 선택 </InfoText>
+				<InfoText>[ESC] 닫기</InfoText>
+			</ShortcutInfo>
 		</Container>
 	);
 }
