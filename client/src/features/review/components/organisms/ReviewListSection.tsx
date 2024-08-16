5bdf005c228abaa50236dd25cfe9cea6a7f418d0
icallitnewart <icallitnewart@gmail.com>
2024-08-16 10:44:00 +0900

    [FE] Refactor: 리뷰 목록 페이지의 리뷰 목록 섹션 컴포넌트로 분리
    설명
    - 리뷰 목록 섹션 ReviewListSection 컴포넌트로 분리
    
    Related to: #28

---
+++ client/src/features/review/components/organisms/ReviewListSection.tsx
@@ -0,0 +1,46 @@
+import React, { useMemo } from 'react';
+import styled from 'styled-components';
+
+import { SortTypes } from '@config/query/queryKeys';
+
+import useCalculatePerPage from '@hooks/useCalculatedPerPage';
+import useReviewListInfinite from '../../hooks/useReviewListInfinite';
+
+import ReviewCardList from './ReviewCardList';
+
+const Container = styled.section`
+	display: flex;
+	flex-wrap: wrap;
+	gap: 30px;
+	width: 100%;
+	padding: 60px 0px 80px;
+`;
+
+function ReviewListSection(): JSX.Element {
+	const perPage = useCalculatePerPage({
+		itemHeight: 400,
+		itemsPerRow: 3,
+	});
+	const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+		useReviewListInfinite({
+			sort: { type: SortTypes.LATEST },
+			pagination: { perPage },
+		});
+
+	const reviews = useMemo(() => {
+		return data?.pages.flatMap(page => page.reviews) || [];
+	}, [data]);
+
+	return (
+		<Container>
+			<ReviewCardList
+				reviews={reviews}
+				fetchNextPage={fetchNextPage}
+				hasNextPage={hasNextPage}
+				isFetchingNextPage={isFetchingNextPage}
+			/>
+		</Container>
+	);
+}
+
+export default ReviewListSection;
