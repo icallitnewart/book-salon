54ed1c3714fc3f331b398b805ad028fedf1b804f
icallitnewart <icallitnewart@gmail.com>
2024-08-18 22:24:25 +0900

    [FE] Refactor: Header의 마이페이지 NavLink 활성화 조건 설정
    설명
    - 마이페이지로 분류되는 내 프로필 조회, 회원정보 수정, 찜한 도서 페이지 접속시 Header의 MYPAGE NavLink가 활성화되어 active 스타일이 적용되도록 설정
    
    Related to: #31, #7

--- client/src/shared/components/atoms/MenuLink.tsx
+++ client/src/shared/components/atoms/MenuLink.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Menu = styled(NavLink)`
-	color: #333;
+interface IMenuStyleProps {
+	$isActive?: boolean;
+}
+
+const Menu = styled(NavLink)<IMenuStyleProps>`
+	color: ${({ $isActive }) =>
+		$isActive ? 'var(--sub-color-darkgreen)' : '#333'};
 	font-family: var(--main-font-eng);
 	font-size: 16px;
 	font-weight: 500;
@@ -20,13 +25,17 @@ const Menu = styled(NavLink)`
 	}
 `;
 
-interface IMenuLinkProps {
+interface IMenuLinkProps extends IMenuStyleProps {
 	children: string;
 	to: string;
 }
 
-function MenuLink({ children, to }: IMenuLinkProps): JSX.Element {
-	return <Menu to={to}>{children}</Menu>;
+function MenuLink({ children, to, $isActive }: IMenuLinkProps): JSX.Element {
+	return (
+		<Menu to={to} $isActive={$isActive}>
+			{children}
+		</Menu>
+	);
 }
 
 export default MenuLink;
--- client/src/shared/components/molecules/Navigation.tsx
+++ client/src/shared/components/molecules/Navigation.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { ROUTES } from '@constants/routes';
@@ -15,15 +16,27 @@ const Container = styled.nav`
 `;
 
 function Navigation(): JSX.Element {
+	const location = useLocation();
 	const { data: isAuth } = useAuthUser({
 		select: data => data.isAuth,
 	});
+
+	const isActiveMyPage = useMemo(() => {
+		const activeRoutes = [
+			ROUTES.USER.MY_PROFILE,
+			ROUTES.USER.PROFILE_EDIT,
+			ROUTES.BOOK.LIKED_LIST,
+		];
+		return !!activeRoutes.find(route => route === location.pathname);
+	}, [location]);
 
 	return (
 		<Container>
 			<MenuLink to={ROUTES.REVIEW.LIST}>REVIEWS</MenuLink>
 			{isAuth ? (
-				<MenuLink to={ROUTES.USER.MY_PROFILE}>MYPAGE</MenuLink>
+				<MenuLink to={ROUTES.USER.MY_PROFILE} $isActive={isActiveMyPage}>
+					MYPAGE
+				</MenuLink>
 			) : (
 				<MenuLink to={ROUTES.USER.LOGIN}>LOGIN</MenuLink>
 			)}
