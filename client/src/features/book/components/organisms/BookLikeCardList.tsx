057ffa139953a768fedf4df721b49433593dac55
icallitnewart <icallitnewart@gmail.com>
2024-08-19 09:26:32 +0900

    [FE] Deisgn: 찜한 도서 목록 빈 상태 UI 개발 및 적용
    설명
    - 찜한 도서 목록을 위한 빈 상태 UI 레이아웃 구현 및 디자인 작업
    - 찜한 도서 목록 페이지의 도서 목록 데이터 개수가 0인 경우, 빈 상태 UI를 출력하여 사용자 경험 향상
    
    Related to: #32

--- client/src/features/book/components/organisms/BookLikeCardList.tsx
+++ client/src/features/book/components/organisms/BookLikeCardList.tsx
@@ -8,6 +8,8 @@ import useInfiniteScroll from '@hooks/useInfiniteScroll';
 import useCalculatePerPage from '@hooks/useCalculatedPerPage';
 import useLikedBookList from '@features/book/hooks/useLikedBookList';
 
+import { Paragraph } from '@typographies';
+import { ReactComponent as BookSvg } from '@assets/svg/book.svg';
 import withAsyncBoundary from '@components/organisms/withAsyncBoundary';
 import withPaginationObserver from '@components/organisms/withPaginationObserver';
 import BookCardItem from '../molecules/BookCardItem';
@@ -17,10 +19,11 @@ const Container = styled.div`
 	gap: 20px 50px;
 	flex-wrap: wrap;
 	width: 100%;
+	min-height: 300px;
 	padding: 20px;
 `;
 
-const ObserverContainer = styled.li`
+const ObserverContainer = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -37,7 +40,6 @@ function BookLikeCardList(): JSX.Element {
 	});
 	const { books, fetchNextPage, hasNextPage, isFetchingNextPage } =
 		useLikedBookList({ pagination: { perPage } });
-
 	const observerRef = useInfiniteScroll({
 		hasNextPage: !!hasNextPage,
 		isFetchingNextPage: !!isFetchingNextPage,
@@ -52,24 +54,55 @@ function BookLikeCardList(): JSX.Element {
 		loader: <BookLikeCardList.Skeleton />,
 	});
 
+	if (!books || books.length === 0) {
+		return <BookLikeCardList.EmptyAlert />;
+	}
+
 	return (
 		<Container>
-			{books &&
-				books.map(book => (
-					<BookCardItem
-						key={book.isbn}
-						title={book.title}
-						author={book.author}
-						cover={book.cover}
-						link={ROUTES.BOOK.DETAIL(book.isbn)}
-					/>
-				))}
+			{books.map(book => (
+				<BookCardItem
+					key={book.isbn}
+					title={book.title}
+					author={book.author}
+					cover={book.cover}
+					link={ROUTES.BOOK.DETAIL(book.isbn)}
+				/>
+			))}
 			{hasNextPage && <PaginationObserver />}
 		</Container>
 	);
 }
 
-BookLikeCardList.Skeleton = function () {
+const CentredWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	flex: 1;
+`;
+
+const BookIcon = styled(BookSvg)`
+	width: 50px;
+	height: 50px;
+	color: #bbb;
+	margin-bottom: 10px;
+`;
+
+BookLikeCardList.EmptyAlert = function (): JSX.Element {
+	return (
+		<Container>
+			<CentredWrapper>
+				<BookIcon />
+				<Paragraph variant="article-subtitle-sm" $color="#aaa">
+					좋아요한 도서가 없습니다.
+				</Paragraph>
+			</CentredWrapper>
+		</Container>
+	);
+};
+
+BookLikeCardList.Skeleton = function (): JSX.Element {
 	return (
 		<Container>
 			{Array.from({ length: ITEMS_PER_ROW * 2 }).map(_ => (
