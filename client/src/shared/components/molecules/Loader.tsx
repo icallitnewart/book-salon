337fc1ef1a57f592340435ae182b0663f68da4f0
icallitnewart <icallitnewart@gmail.com>
2024-08-22 16:47:12 +0900

    [FE] Design: 버튼 Loader UI 개발
    설명
    - 기존의 Loader 컴포넌트를 사용하여 버튼을 위한 Loader 디자인 작업
    
    Related to: #32

--- client/src/shared/components/molecules/Loader.tsx
+++ client/src/shared/components/molecules/Loader.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { keyframes, styled } from 'styled-components';
 
+const spinnerSize = {
+	sm: 20,
+	md: 50,
+	lg: 80,
+};
+
 interface ISpinnerStyleProps {
-	$size?: number;
+	$size?: 'sm' | 'md' | 'lg';
+	$isDarkBg?: boolean;
 }
 
 const spinningAnimation = keyframes`
@@ -19,25 +26,29 @@ const Spinner = styled.div<ISpinnerStyleProps>`
 	font-size: 10px;
 	position: relative;
 	text-indent: -9999em;
-	border-top: 1em solid rgba(209, 209, 209, 0.2);
-	border-right: 1em solid rgba(209, 209, 209, 0.2);
-	border-bottom: 1em solid rgba(209, 209, 209, 0.2);
-	border-left: 1em solid #d1d1d1;
+	border-top: 5px solid rgba(209, 209, 209, 0.2);
+	border-right: 5px solid rgba(209, 209, 209, 0.2);
+	border-bottom: 5px solid rgba(209, 209, 209, 0.2);
+	border-left: 5px solid
+		${({ $isDarkBg }) => ($isDarkBg ? '#fdfdfd' : '#d1d1d1')};
 	transform: translateZ(0);
 	animation: ${spinningAnimation} 1.1s infinite linear;
 
 	&,
 	&:after {
 		border-radius: 50%;
-		width: ${({ $size }) => $size}em;
-		height: ${({ $size }) => $size}em;
+		width: ${({ $size }) => ($size ? spinnerSize[$size] : spinnerSize.md)}px;
+		height: ${({ $size }) => ($size ? spinnerSize[$size] : spinnerSize.md)}px;
 	}
 `;
 
 type ILoaderProps = ISpinnerStyleProps;
 
-function Loader({ $size = 4 }: ILoaderProps): JSX.Element {
-	return <Spinner $size={$size} />;
+function Loader({
+	$size = 'md',
+	$isDarkBg = false,
+}: ILoaderProps): JSX.Element {
+	return <Spinner $size={$size} $isDarkBg={$isDarkBg} />;
 }
 
 export default Loader;
